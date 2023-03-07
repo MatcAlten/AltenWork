a862e29fdeeb3f75f66998b0faa9d21b1d49c178
MatcAlten <matteo.chiadini@alten.it>
2023-03-07 10:09:51 +0100

    agg

--- Crud/Crud/src/main/java/com/exercise/Crud/repository/MangaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.exercise.Crud.repository;
-
-import com.exercise.Crud.model.Manga;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MangaRepo extends JpaRepository <Manga, Integer> {
-}
